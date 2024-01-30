021d9beebfc6eb1320d5a81de74a4a6ac95a3147
Keegan Royal-Eisenberg <kroyal.eisenberg@gmail.com>
2024-01-29 22:04:03 -0800

    Added API back in and began working on dynamic html creation in JS.

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,34 +1,23 @@
 var main = $("main");
 
 function getApi() {
-	var requestUrl = "api.openweathermap.org/data/2.5/forecast";
+	var requestUrl =
+		"https://api.openweathermap.org/data/2.5/forecast?lat=40&lon=74&appid=2ecc56f6f728983cbd84a7025a8bc07e";
 
-	$(function () {
-		fetch(requestUrl)
-			.then(function (response) {
-				return response.json();
-			})
-			.then(function (data) {
-				console.log(data);
-				//Loop over the data to generate a table, each table row will have a link to the repo url
-				for (var i = 0; i < data.length; i++) {
-					// Creating elements, tablerow, tabledata, and anchor
-					var createTableRow = document.createElement("tr");
-					var tableData = document.createElement("td");
-					var link = document.createElement("a");
-
-					// Setting the text of link and the href of the link
-					link.textContent = data[i].html_url;
-					link.href = data[i].html_url;
-
-					// Appending the link to the tabledata and then appending the tabledata to the tablerow
-					// The tablerow then gets appended to the tablebody
-					tableData.append(link);
-					createTableRow.append(tableData);
-					main.append(createTableRow);
-				}
-			});
-	});
+	fetch(requestUrl)
+		.then(function (response) {
+			if (!response.ok) {
+				return response.status;
+			}
+			return response.json();
+		})
+		.then(function (data) {
+			console.log(data);
+			$("#schedule").text(data.city.name);
+		})
+		.catch(function (err) {
+			console.log(err);
+		});
 }
 
 getApi();
