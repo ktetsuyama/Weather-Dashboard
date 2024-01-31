e36abcf1f7fe03d214d094d4919c7a8750c8ffe7
Keegan Royal-Eisenberg <kroyal.eisenberg@gmail.com>
2024-01-30 19:38:25 -0800

    Added in a search bar and local storage.

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,8 +1,25 @@
-var main = $("main");
+// var today = dayjs();
 
 function getApi() {
 	var requestUrl =
 		"https://api.openweathermap.org/data/2.5/forecast?lat=40&lon=74&appid=2ecc56f6f728983cbd84a7025a8bc07e";
+
+	var searchBar = $("<input>")
+		.attr("type", "text")
+		.attr("id", "searchBoxInput")
+		.attr("placholder", "Search...")
+		.attr("class", "col-2 col-md-1 hour text-center py-3")
+		.val(localStorage.getItem("searchText"));
+
+	$("#weather").append(searchBar);
+
+	var searchBarButton = $("<button>").attr("id", "searchButton").text("Search");
+	$("#weather").append(searchBarButton);
+
+	$("#searchButton").click(function () {
+		var textInput = $("#searchBoxInput").val();
+		localStorage.setItem("searchText", textInput);
+	});
 
 	fetch(requestUrl)
 		.then(function (response) {
@@ -13,7 +30,7 @@ function getApi() {
 		})
 		.then(function (data) {
 			console.log(data);
-			$("#schedule").text(data.city.name);
+			// $("#weather").text(data.city.name);
 		})
 		.catch(function (err) {
 			console.log(err);
