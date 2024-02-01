afa2f3b12d32ac45d1c3b14dc174734d5ad1de88
Keegan Royal-Eisenberg <kroyal.eisenberg@gmail.com>
2024-02-01 01:42:34 -0800

    Removed helper CSS, finished JS for single city search, polished HTML.

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -8,6 +8,8 @@ function getApi() {
 		}
 		localStorage.setItem("searchText", textInput);
 		$("#searchBoxInput").val("");
+		$("#today").empty();
+		$("#fiveDay").empty();
 
 		var previousSearch = $("<button>")
 			.attr("class", "pastSearchBtn col-12 btn btn-info my-1")
@@ -44,17 +46,19 @@ function getApi() {
 				var kWindSpeed = data.list[0].wind.speed;
 				var kLocation = data.city.name;
 				var iconElement = $("<img>").attr("src", iconUrl);
+				var divider = $("<hr>");
 				var mainDisplay = $("<div>")
 					.attr("id", "mD")
-					.attr("class", "col-12 text-start py-3")
-					.html(
-						"Today in " +
-							kLocation +
-							" " +
-							today.format("dddd, MMM D, YYYY ") +
-							iconElement[0].outerHTML
-					);
+					.attr("class", "border border-primary col-12 text-start py-3");
 				$(mainDisplayDiv).append(mainDisplay);
+
+				var mainDisplayHead = $("<h4>").text(
+					"Today in " + kLocation + " " + today.format("dddd, MMM D, YYYY ")
+				);
+				$(mainDisplay).append(mainDisplayHead);
+
+				var icon5D = $("<div>").html(iconElement[0].outerHTML);
+				$(mainDisplay).append(icon5D);
 
 				var temperature = $("<p>").text("Temp: " + kTemp + "°F");
 				$(mainDisplay).append(temperature);
@@ -64,37 +68,52 @@ function getApi() {
 
 				var weather = $("<p>").text("Humidity: " + kHumidity + "%");
 				$(mainDisplay).append(weather);
-				var fiveDayTitle = $("<h4>").text("5 Day Forecast");
 
+				var fiveDayTitle = $("<h4>")
+					.attr("class", "pt-3")
+					.text("5 Day Forecast");
 				$("#fiveDay").append(fiveDayTitle);
+				$("#fiveDay").append(divider);
 
 				$("<div>");
 				for (var i = 0; i <= 4; i++) {
 					var forecastId = "fD" + timestamp + "_" + i;
 					var forecastDate = dayjs()
 						.add(i + 1, "days")
-						.format("dddd, MMM D, YYYY");
+						.format("MMM D, YYYY");
 
-					var forecastDisplay = $("<card>")
+					var forecastDisplayCard = $("<card>")
+						.attr("id", "fDC")
+						.attr(
+							"class",
+							"card text-white bg-secondary justify-content-center col-2 p-1 mx-2"
+						);
+					$("#fiveDay").append(forecastDisplayCard);
+
+					var forecastDisplayHeader = $("<h5>")
 						.attr("id", forecastId)
-						.attr("class", "col-2 mx-2")
+						.attr("class", "card-title")
 						.text(forecastDate);
-					$("#fiveDay").append(forecastDisplay);
-					var icon5D = $("<div>").html(iconElement[0].outerHTML);
-					$(forecastDisplay).append(icon5D);
+					$(forecastDisplayCard).append(forecastDisplayHeader);
 
-					var temperature = $("<p>").text(
-						"Temp: " + data.list[i].main.temp + "°F"
-					);
-					$(forecastDisplay).append(temperature);
-					var windSpeed = $("<p>").text(
-						"Wind speed: " + data.list[i].wind.speed + "MPH"
-					);
-					$(forecastDisplay).append(windSpeed);
-					var weather = $("<p>").text(
-						"Humidity: " + data.list[i].main.humidity + "%"
-					);
-					$(forecastDisplay).append(weather);
+					var forecastDisplayBody = $("<div>").attr("class", "card-body px-0");
+					$(forecastDisplayHeader).append(forecastDisplayBody);
+
+					var icon5D = $("<div>").html(iconElement[0].outerHTML);
+					$(forecastDisplayBody).append(icon5D);
+
+					var temperature = $("<p>")
+						.attr("class", "h6 card-text text-left")
+						.text("Temp: " + data.list[i].main.temp + "°F");
+					$(forecastDisplayBody).append(temperature);
+					var windSpeed = $("<p>")
+						.attr("class", "h6 card-text text-left")
+						.text("Wind speed: " + data.list[i].wind.speed + "MPH");
+					$(forecastDisplayBody).append(windSpeed);
+					var weather = $("<p>")
+						.attr("class", "h6 card-text text-left")
+						.text("Humidity: " + data.list[i].main.humidity + "%");
+					$(forecastDisplayBody).append(weather);
 				}
 			})
 			.catch(function (err) {
